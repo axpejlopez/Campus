96b8961fa1e65418626699e96a1af019d6855363
axpejlopez <164158630+axpejlopez@users.noreply.github.com>
2025-10-20 08:54:33 +0200

    First commit with estructure

---
+++ src/main/java/com/example/campusmanager/CampusmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.campusmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusmanagerApplication.class, args);
+	}
+
+}
