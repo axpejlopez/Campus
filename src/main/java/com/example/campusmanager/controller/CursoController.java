65f1610dbfc5cc06bd9a72a4bcd6001c4270fd51
kike1735 <supermoorgan89@gmail.com>
2025-10-20 11:52:31 +0200

    creado todos los endpoint de cursos, cambiado el pom para añadir @notblank
    creado todos los endpoint de cursos, cambiado el pom para añadir @notblank
    testeado con Junit
    ejecutado Postman de manera satisfactoria

---
+++ src/main/java/com/example/campusmanager/controller/CursoController.java
@@ -0,0 +1,45 @@
+package com.example.campusmanager.controller;
+
+
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.campusmanager.domain.Curso;
+import com.example.campusmanager.service.CursoService;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/cursos")
+public class CursoController {
+
+    private final CursoService service;
+
+    public CursoController(CursoService service) {
+        this.service = service;
+    }
+
+    // GET /api/cursos -> lista todos
+    @GetMapping
+    public List<Curso> getAll() {
+        return service.listarTodos();
+    }
+
+    // POST /api/cursos -> crea nuevo curso
+    @PostMapping
+    public ResponseEntity<?> crear(@RequestBody Curso c) {
+        try {
+            Curso creado = service.crear(c);
+            return ResponseEntity
+                    .created(URI.create("/api/cursos/" + creado.getId()))
+                    .body(creado);
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.badRequest().body(Map.of("message", ex.getMessage()));
+        }
+    }
+}
+
