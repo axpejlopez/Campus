2b8723cc20b9e9f22197271404678be18b90fe1e
kike1735 <supermoorgan89@gmail.com>
2025-10-22 11:32:55 +0200

    Update CampusmanagerApplication.java

--- src/main/java/com/example/campusmanager/CampusmanagerApplication.java
+++ src/main/java/com/example/campusmanager/CampusmanagerApplication.java
@@ -3,7 +3,7 @@ package com.example.campusmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.example.campusmanager")
+@SpringBootApplication
 public class CampusmanagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CampusmanagerApplication.class, args);
